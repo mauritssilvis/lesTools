8fd870a752c377e10afe9acc1ae3b98a540f0e5b
Maurits Silvis <mauritssilvis@gmail.com>
2019-04-01 00:36:23 +0200

    Restructure test scripts

--- modStats/test/auxiliary/testCompAvg.m
+++ modStats/test/auxiliary/testCompAvg.m
@@ -1,256 +1,463 @@
 % Test compAvg()
 
-% Define parameters
-nDims1 = 1;
-nDims2 = 2;
-nDims3 = 3;
-
-nVals1 = 1;
-nVals2 = 2;
-nVals3 = 3;
-
 %% Test output type #1
-nDims = nDims1;
-nVals = nVals1;
+% Define parameters
+nDims = 1;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #2
-nDims = nDims1;
-nVals = nVals2;
+% Define parameters
+nDims = 1;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #3
-nDims = nDims1;
-nVals = nVals3;
+% Define parameters
+nDims = 1;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #4
-nDims = nDims2;
-nVals = nVals1;
+% Define parameters
+nDims = 2;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #5
-nDims = nDims2;
-nVals = nVals2;
+% Define parameters
+nDims = 2;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #6
-nDims = nDims2;
-nVals = nVals3;
+% Define parameters
+nDims = 2;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #7
-nDims = nDims3;
-nVals = nVals1;
+% Define parameters
+nDims = 3;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #8
-nDims = nDims3;
-nVals = nVals2;
+% Define parameters
+nDims = 3;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output type #9
-nDims = nDims3;
-nVals = nVals3;
+% Define parameters
+nDims = 3;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-avg = compAvg(vals);
+
+% Obtain value
+val = compAvg(vals);
+
+% Define expected type
 expType = 'double';
-assert( isa(avg, expType), 'Output not of expected type ''double''.');
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
 %% Test output size #1
-nDims = nDims1;
-nVals = nVals1;
+% Define parameters
+nDims = 1;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #2
-nDims = nDims1;
-nVals = nVals2;
+% Define parameters
+nDims = 1;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #3
-nDims = nDims1;
-nVals = nVals3;
+% Define parameters
+nDims = 1;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #4
-nDims = nDims2;
-nVals = nVals1;
+% Define parameters
+nDims = 2;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #5
-nDims = nDims2;
-nVals = nVals2;
+% Define parameters
+nDims = 2;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #6
-nDims = nDims2;
-nVals = nVals3;
+% Define parameters
+nDims = 2;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #7
-nDims = nDims3;
-nVals = nVals1;
+% Define parameters
+nDims = 3;
+nVals = 1;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #8
-nDims = nDims3;
-nVals = nVals2;
+% Define parameters
+nDims = 3;
+nVals = 2;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test output size #9
-nDims = nDims3;
-nVals = nVals3;
+% Define parameters
+nDims = 3;
+nVals = 3;
 vals = ones(nDims, nDims, nVals);
-actSize = size( compAvg(vals) );
+
+% Obtain value
+val = compAvg(vals);
+
+% Obtain actual size
+actSize = size( val );
+
+% Define expected size
 expSize = [nDims, nDims];
-assert( all(expSize == actSize), 'Output not of expected size.');
+
+% Test
+assert( all(actSize == expSize), 'Output not of expected size.');
 
 %% Test actual output #1
-nDims = nDims1;
-nVals = nVals1;
+% Define parameters
+nDims = 1;
+nVals = 1;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = 1;
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = 1;
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #2
-nDims = nDims1;
-nVals = nVals2;
+% Define parameters
+nDims = 1;
+nVals = 2;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = 1.5;
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = 1.5;
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #3
-nDims = nDims1;
-nVals = nVals3;
+% Define parameters
+nDims = 1;
+nVals = 3;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = 2;
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = 2;
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #4
-nDims = nDims2;
-nVals = nVals1;
+% Define parameters
+nDims = 2;
+nVals = 1;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
     1, 3;
     2, 4;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #5
-nDims = nDims2;
-nVals = nVals2;
+% Define parameters
+nDims = 2;
+nVals = 2;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
     3, 5;
     4, 6;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #6
-nDims = nDims2;
-nVals = nVals3;
+% Define parameters
+nDims = 2;
+nVals = 3;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
     5, 7;
     6, 8;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #7
-nDims = nDims3;
-nVals = nVals1;
+% Define parameters
+nDims = 3;
+nVals = 1;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
     1, 4, 7;
     2, 5, 8;
     3, 6, 9;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #8
-nDims = nDims3;
-nVals = nVals2;
+% Define parameters
+nDims = 3;
+nVals = 2;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
      5.5,  8.5, 11.5;
      6.5,  9.5, 12.5;
      7.5, 10.5, 13.5;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
 
 %% Test actual output #9
-nDims = nDims3;
-nVals = nVals3;
+% Define parameters
+nDims = 3;
+nVals = 3;
 vals = zeros(nDims, nDims, nVals);
 vals(:) = 1 : nDims * nDims * nVals;
-actAvg = compAvg(vals);
-expAvg = [
+
+% Obtain actual value
+actVal = compAvg(vals);
+
+% Define expected value
+expVal = [
     10, 13, 16;
     11, 14, 17;
     12, 15, 18;
 ];
-assert( all( all( all( expAvg == actAvg ) ) ), 'Unexpected output.');
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output.');
