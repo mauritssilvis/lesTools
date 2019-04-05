fb6aa5c5a88514a230780bb2f060cf041ff9a6d8
Maurits Silvis <mauritssilvis@gmail.com>
2019-04-05 17:06:55 +0200

    Add tests

--- modStats/test/auxiliary/testCheckGradsFun.m
+++ modStats/test/auxiliary/testCheckGradsFun.m
@@ -56,6 +56,34 @@ expType = 'double';
 % Test
 assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
 
+%% Test output type #5
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
+spaceDims = 2;
+
+% Obtain value
+val = checkGradsFun(gradsFun, spaceDims);
+
+% Define expected type
+expType = 'double';
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
+
+%% Test output type #6
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
+spaceDims = 3;
+
+% Obtain value
+val = checkGradsFun(gradsFun, spaceDims);
+
+% Define expected type
+expType = 'double';
+
+% Test
+assert( isa(val, expType), ['Output not of expected type ''', expType, '''.']);
+
 %% Test output size #1
 % Define parameters
 gradsFun = 'unifMats';
@@ -128,6 +156,42 @@ expSize = [1, 1, 1];
 % Test
 assert( numel( size(val) ) <= numel(expSize) && all(actSize == expSize), 'Output not of expected size.');
 
+%% Test output size #5
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
+spaceDims = 2;
+
+% Obtain value
+val = checkGradsFun(gradsFun, spaceDims);
+
+% Obtain actual size
+actSize = [size(val, 1), size(val, 2), size(val, 3)];
+
+
+% Define expected size
+expSize = [1, 1, 1];
+
+% Test
+assert( numel( size(val) ) <= numel(expSize) && all(actSize == expSize), 'Output not of expected size.');
+
+%% Test output size #6
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
+spaceDims = 3;
+
+% Obtain value
+val = checkGradsFun(gradsFun, spaceDims);
+
+% Obtain actual size
+actSize = [size(val, 1), size(val, 2), size(val, 3)];
+
+
+% Define expected size
+expSize = [1, 1, 1];
+
+% Test
+assert( numel( size(val) ) <= numel(expSize) && all(actSize == expSize), 'Output not of expected size.');
+
 %% Test actual output #1
 % Define parameters
 gradsFun = 'unifMats';
@@ -173,6 +237,34 @@ assert( all( all( all(actVal == expVal) ) ), 'Unexpected output');
 %% Test actual output #4
 % Define parameters
 gradsFun = 'normMats';
+spaceDims = 3;
+
+% Obtain actual value
+actVal = checkGradsFun(gradsFun, spaceDims);
+
+% Define expected value
+expVal = 0;
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output');
+
+%% Test actual output #5
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
+spaceDims = 2;
+
+% Obtain actual value
+actVal = checkGradsFun(gradsFun, spaceDims);
+
+% Define expected value
+expVal = 0;
+
+% Test
+assert( all( all( all(actVal == expVal) ) ), 'Unexpected output');
+
+%% Test actual output #6
+% Define parameters
+gradsFun = @(sampleNr, nGrads, spaceDims) reshape(1 : spaceDims * spaceDims * nGrads, spaceDims, spaceDims, nGrads);
 spaceDims = 3;
 
 % Obtain actual value
